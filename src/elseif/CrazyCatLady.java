b83141039a146a55887a656379e3aa24277fc935
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-01-23 19:17:22 -0800

    crazy cat lady robot color chosser

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -15,17 +15,18 @@ public class CrazyCatLady {
 	int ocs=Integer.parseInt(face);
 		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
 	if (ocs >3) {
+
 	JOptionPane.showMessageDialog(null,"you are a crazy cat lady");
 	}
 		// 4. If they have 3 or less, call the method below to show them a cat video
 	
-	if (ocs<3)
+	if (ocs<3) {
+	playVideo("https://www.youtube.com/watch?v=QH2-TGUlwu4");
 	
 	
 	
 	
-	
-	
+	}
 		// 5.parentComponent If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
 		
 	}
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -5,23 +5,61 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+Robot r2d2= new Robot();
 
-		//3. ask the user what color they would like the robot to draw
+	for (int i = 0; i < 8; i++) {
+	
 		
+	
+		//3. ask the user what color they would like the robot to draw
+		String color= JOptionPane.showInputDialog("What color do you want");
 		//4. use an if/else statement to set the pen color that the user requested
-
+if(color.equals ("blue")) {
+	r2d2.setPenColor(Color.blue);
+}	
+	else if (color.equals ("red"))	{
+		r2d2.setPenColor(Color.red);
+		
+		}
+		else if (color.equals ("yellow")) {
+			r2d2.setPenColor(Color.YELLOW);
+		}
+		
         //5. if the user doesn’t enter anything, choose a random color
-
+		else {
+			r2d2.setRandomPenColor();
+		}
         //6. put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//2. set the pen width to 10
-		
+		r2d2.setPenWidth(10);
 	    //1. make the robot draw a shape (this will take more than one line of code)
 
+		
 
+
+r2d2.setSpeed(50);
+r2d2.penDown();
+r2d2.turn(270);
+r2d2.move(100);
+r2d2.turn(90);
+r2d2.move(200);
+r2d2.turn(90);
+r2d2.move(200);
+r2d2.turn(90);
+r2d2.move(200);
+r2d2.turn(90);
+r2d2.move(100);
+
+
+
+	}
 	}
 }
