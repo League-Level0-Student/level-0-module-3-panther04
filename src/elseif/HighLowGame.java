25167bdda423c868d274270d14bf416a82eb0d4e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-06 19:20:39 -0800

    high low game

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,6 +5,8 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
@@ -15,7 +17,7 @@ public class HighLowGame {
 		// 11. do the following 10 times
 		
 			// 1. ask the user for a guess using a pop-up window, and save their response 
-
+			String matt = JOptionPane.showInputDialog("Guess what is 1000*5646576");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
 			
 			// 5. if the guess is correct
