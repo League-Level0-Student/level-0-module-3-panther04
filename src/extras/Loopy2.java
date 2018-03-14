1b16e56ebeec7e3a3f359be7a29c39381b8ec6b2
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-13 19:19:22 -0700

    loopy2 loopy spud

---
+++ src/extras/Loopy2.java
@@ -0,0 +1,12 @@
+package extras;
+
+import javax.swing.JOptionPane;
+
+public class Loopy2 {
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 500; i = i + 2) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ src/extras/spud.java
@@ -0,0 +1,22 @@
+package extras;
+
+public class spud {
+	public static void main(String[] args) {
+		
+		
+		
+		for(int i = 1; i <=8; i++) {
+			
+		
+		
+		
+		System.out.println(i);
+		
+		
+		
+		
+		
+		
+	}
+}
+}
